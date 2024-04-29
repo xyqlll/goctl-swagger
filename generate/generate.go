9f471c428f5139589064a09bfe598fc24a296f60
jiayuguang <jiayuguang@hudiexiaoying.cn>
2024-04-29 17:18:13 +0800

    openapi

--- generate/generate.go
+++ generate/generate.go
@@ -5,6 +5,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"net/http"
+	"strings"
 
 	"github.com/zeromicro/go-zero/tools/goctl/plugin"
 )
@@ -24,9 +26,46 @@ func Do(filename string, host string, basePath string, schemes string, in *plugi
 
 	output := in.Dir + "/" + filename
 
-	err = ioutil.WriteFile(output, formatted.Bytes(), 0666)
+	data, err := GetOpenApi3Data(formatted.Bytes())
+	if err != nil {
+		return err
+	}
+
+	err = ioutil.WriteFile(output, data, 0666)
 	if err != nil {
 		fmt.Println(err)
 	}
 	return err
+}
+
+func GetOpenApi3Data(formatted []byte) ([]byte, error) {
+	url := "https://converter.swagger.io/api/convert"
+	method := "POST"
+
+	payload := strings.NewReader(string(formatted))
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, payload)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	req.Header.Add("priority", "u=1, i")
+	req.Header.Add("content-type", "application/json")
+	req.Header.Add("Accept", "*/*")
+	req.Header.Add("Host", "converter.swagger.io")
+	req.Header.Add("Connection", "keep-alive")
+
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(body))
+
+	return body, nil
 }
